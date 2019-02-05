6fd8e5b2dba2f8bf081b8332b7d3612df94f29cf
Merlinebene <47316167+Merlinebene@users.noreply.github.com>
2019-02-05 15:02:03 +0530

    Create Alphabet.py

---
+++ Alphabet.py
@@ -0,0 +1,6 @@
+ch = input()
+if(ch=='A' or ch=='a' or ch=='B' or ch=='b' or ch=='c' or ch=='C' or ch=='D' or ch=='d' or ch=='E' or ch =='e' or ch=='F' or ch=='f' or ch=='I'
+ or ch=='i' or ch=='J' or ch=='j' or ch=='k' or ch=='K' or ch=='L' or ch=='l' or ch=='M' or ch=='m' or  ch=='N' or ch=='n' or ch=='O' or ch=='o' or ch=='P' or ch=='p' or ch=='Q' or ch=='q' or ch=='R' or ch=='r' or ch=='S' or ch=='s' or ch=='T' or ch=='t' or ch=='U' or ch=='u' or ch=='V' or ch=='v' or ch=='W' or ch=='w' or ch=='X' or ch=='x' or ch=='Y' or ch=='y' or ch=='Z' or ch=='z'):
+    print("Alphabet")
+else:
+    print("No")
