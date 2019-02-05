c33e71f80c1e74867369e72f7bc300e33792497b
Merlinebene <47316167+Merlinebene@users.noreply.github.com>
2019-02-05 14:48:48 +0530

    Update Vowel or consonant.py

--- Vowel or consonant.py
+++ Vowel or consonant.py
@@ -1,5 +1,6 @@
-ch = input("Enter a character: ")
-if(ch=='a','e','i','o','u'):
-    print(ch, "Vowel")
+ch = input()
+if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
+ or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+    print("Vowel")
 else:
-    print(ch, "Consonant")
+    print("Consonant")
